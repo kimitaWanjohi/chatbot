bb921d2688ce39173d2285a2618de4bb8570cf18
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-06-21 13:31:13 +0300

    views

--- bot/openai/main.py
+++ bot/openai/main.py
@@ -1,18 +1,17 @@
-import os
+from django.conf import settings
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = settings.OPENAI_API_KEY
 
-start_sequence = "\nAI:"
-restart_sequence = "\nHuman: "
-
-response = openai.Completion.create(
-  model="text-davinci-002",
-  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=[" Human:", " AI:"]
-)
+def init_chat(prompt):
+    response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=prompt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+    return response
